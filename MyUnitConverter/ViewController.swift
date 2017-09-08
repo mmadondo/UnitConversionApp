55dd0ec7051aab46a7d4852ba46f6329de1f2231
Malvern Madondo <malvernmadondo@Malverns-MacBook-Pro.local>
2017-09-08 09:49:09 -0500

    Added temp converson code

--- MyUnitConverter/ViewController.swift
+++ MyUnitConverter/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var FarhenheitLabel: UITextField!
 
+    @IBOutlet weak var CalculationLabel: UILabel!
+    @IBOutlet weak var CelsiusLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blue  //background color
+        
     }
 
+    @IBAction func didTapConvert(_ sender: Any) {
+        let tempF = Double(FarhenheitLabel.text!) ?? 0
+        let tempC = (tempF - 32.0) / 1.8
+        
+        CelsiusLabel.text = String(format: "%.1f", tempC)
+        let calculation = "(" + FarhenheitLabel.text! + " - 32.0)" + "/ 1.8"
+        CalculationLabel.text = calculation + " = " + CelsiusLabel.text!
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
